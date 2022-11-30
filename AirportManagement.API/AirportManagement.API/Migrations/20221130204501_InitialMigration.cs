d93bf9b5273de495e86c817bb2724d614023c582
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-12-01 01:45:56 +0500

    Migrations Added

---
+++ AirportManagement.API/AirportManagement.API/Migrations/20221130204501_InitialMigration.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirportManagement.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Airlines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AirlineCode = table.Column<int>(type: "INTEGER", nullable: false),
+                    AirlineName = table.Column<string>(type: "TEXT", nullable: false),
+                    AirlineCountry = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Airlines", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Airports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AirportName = table.Column<string>(type: "TEXT", nullable: false),
+                    Country = table.Column<string>(type: "TEXT", nullable: false),
+                    State = table.Column<string>(type: "TEXT", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Airports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Passangers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CountryOfCitizenship = table.Column<string>(type: "TEXT", nullable: false),
+                    CountryOfResidence = table.Column<string>(type: "TEXT", nullable: false),
+                    PassportNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passangers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DepartingGate = table.Column<string>(type: "TEXT", nullable: false),
+                    ArrivingGate = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ArrivingAirportId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DepartingAirportId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AirlineId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AirportId = table.Column<int>(type: "INTEGER", nullable: true),
+                    AirportId1 = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airlines_AirlineId",
+                        column: x => x.AirlineId,
+                        principalTable: "Airlines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airports_AirportId",
+                        column: x => x.AirportId,
+                        principalTable: "Airports",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Flights_Airports_AirportId1",
+                        column: x => x.AirportId1,
+                        principalTable: "Airports",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    BookingPlatform = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PassangerId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PassangersId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Passangers_PassangersId",
+                        column: x => x.PassangersId,
+                        principalTable: "Passangers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NoFlyLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ActiveFrom = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    ActiveTo = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    NoFlyReason = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PassangerId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PassangersId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoFlyLists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NoFlyLists_Passangers_PassangersId",
+                        column: x => x.PassangersId,
+                        principalTable: "Passangers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecurityChecks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CheckResult = table.Column<string>(type: "TEXT", nullable: false),
+                    Comments = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PassangerId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PassangersId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecurityChecks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SecurityChecks_Passangers_PassangersId",
+                        column: x => x.PassangersId,
+                        principalTable: "Passangers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BaggageChecks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CheckResult = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    BookingId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PassangerId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BaggageChecks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BaggageChecks_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Baggages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    WeightInKG = table.Column<double>(type: "REAL", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    BookingId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Baggages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Baggages_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BoardingPasses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    QRCode = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    BookingId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoardingPasses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BoardingPasses_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlightManifests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    FlightId = table.Column<int>(type: "INTEGER", nullable: false),
+                    BookingId = table.Column<int>(type: "INTEGER", nullable: false),
+                    FlightsId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlightManifests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FlightManifests_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FlightManifests_Flights_FlightsId",
+                        column: x => x.FlightsId,
+                        principalTable: "Flights",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaggageChecks_BookingId",
+                table: "BaggageChecks",
+                column: "BookingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Baggages_BookingId",
+                table: "Baggages",
+                column: "BookingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardingPasses_BookingId",
+                table: "BoardingPasses",
+                column: "BookingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_PassangersId",
+                table: "Bookings",
+                column: "PassangersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlightManifests_BookingId",
+                table: "FlightManifests",
+                column: "BookingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlightManifests_FlightsId",
+                table: "FlightManifests",
+                column: "FlightsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_AirlineId",
+                table: "Flights",
+                column: "AirlineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_AirportId",
+                table: "Flights",
+                column: "AirportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_AirportId1",
+                table: "Flights",
+                column: "AirportId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoFlyLists_PassangersId",
+                table: "NoFlyLists",
+                column: "PassangersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurityChecks_PassangersId",
+                table: "SecurityChecks",
+                column: "PassangersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BaggageChecks");
+
+            migrationBuilder.DropTable(
+                name: "Baggages");
+
+            migrationBuilder.DropTable(
+                name: "BoardingPasses");
+
+            migrationBuilder.DropTable(
+                name: "FlightManifests");
+
+            migrationBuilder.DropTable(
+                name: "NoFlyLists");
+
+            migrationBuilder.DropTable(
+                name: "SecurityChecks");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Flights");
+
+            migrationBuilder.DropTable(
+                name: "Passangers");
+
+            migrationBuilder.DropTable(
+                name: "Airlines");
+
+            migrationBuilder.DropTable(
+                name: "Airports");
+        }
+    }
+}
