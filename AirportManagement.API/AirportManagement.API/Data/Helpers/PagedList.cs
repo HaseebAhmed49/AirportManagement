46e0672ebdf52fe2133b008fefcc194f2e3a3d75
HaseebAhmed49 <haseebahmed02@gmail.com>
2023-01-12 23:36:04 +0500

    Working on Pagination for Passangers

---
+++ AirportManagement.API/AirportManagement.API/Data/Helpers/PagedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AirportManagement.API.Data.Helpers
+{
+	public class PagedList<T> : List<T>
+	{
+		public int CurrentPage { get; set; }
+
+		public int TotalPages { get; set; }
+
+		public int PageSize { get; set; }
+
+		public int TotalCount { get; set; }
+	}
+}
+
