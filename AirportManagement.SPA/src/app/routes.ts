d85108ecb783233da51e8f5f97f0acea43adbb40
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-03-25 23:45:51 +0500

    Declared Passanger Post Route

--- AirportManagement.SPA/src/app/routes.ts
+++ AirportManagement.SPA/src/app/routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 import { PassangerDetailsComponent } from "./passanger/passanger-details/passanger-details.component";
+import { PassangerInsertComponent } from "./passanger/passanger-insert/passanger-insert/passanger-insert.component";
 import { PassangerComponent } from "./passanger/passanger.component";
 import { PassangerDetailResolver } from "./_resolvers/passanger-detail.resolver";
 import { PassangerListResolver } from "./_resolvers/passanger-list.resolver";
@@ -8,5 +9,6 @@ import { PassangerListResolver } from "./_resolvers/passanger-list.resolver";
 export const appRoutes: Routes =[
     { path:'passanger', component :PassangerComponent, resolve:{passanger:PassangerListResolver}},
     { path:'passanger/:id', component :PassangerDetailsComponent, resolve:{passanger:PassangerDetailResolver}},
+    { path:'passanger/create', component :PassangerInsertComponent},
 { path:'**', redirectTo : '',pathMatch:'full'}
 ];
